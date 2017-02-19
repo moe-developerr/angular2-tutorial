115206e38d9262dae457ee571cb8d2dc33876a90
moe-developerr <moe.developerr@gmail.com>
2017-02-19 14:38:11 +0200

    routing in progress

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }  from './heroes.component';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroService } from './hero.service';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
